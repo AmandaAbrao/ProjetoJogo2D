a5cbf521bc7109d1000a5b7fca3478e04319758d
AmandaAbrao <amandaabrao55@gmail.com>
2017-10-31 11:10:39 -0200

    Double jump

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -3,122 +3,132 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
-Idle - 0
-Jump -1 
-Run - 2
+
+Idle -0
+Jump - 1
+Run -2
 Falling - 3
-Shooting - 4
-Hurt - 5
- */
+Hurt - 4
 
-
-
+*/ 
 
 public class PlayerCtrl : MonoBehaviour {
 
 
-	public float horizontalSpeed = 10f;
-	public float jumpSpeed = 600f;
-	Rigidbody2D rb;
-	SpriteRenderer sr;
-	Animator anim;
+public float horizontalSpeed = 10f;
+public float jumpSpeed = 600f;
 
-	bool isJumping = false;
+Rigidbody2D rb;
 
-	public Transform feet;
+SpriteRenderer sr;
 
-	public float feetWidth = 0.5f;
+Animator anim;
 
-	public float feetHeigth = 0.1f;
+bool isJumping = false;
 
-	public bool isGrounded;
+public Transform feet;
 
-	public LayerMask whatIsGround;
+public float feetWidth = 0.5f;
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody2D>();
-		sr = GetComponent<SpriteRenderer>();
-		anim = GetComponent<Animator>();
-	}
-	
+public float feetHeight = 0.1f; 
 
-	void OnDrawGizmos() {
-		Gizmos.DrawWireCube(feet.position, new Vector3(feetWidth, feetHeigth, 0f));
-	}
+public bool isGrounded;
+
+public LayerMask whatIsGround;
+
+bool canDoubleJump = false;
+
+public float delayForDoubleJump = 0.2f;
 
 
-	// Update is called once per frame
-	void Update () {
+// Use this for initialization
+void Start () {
+    rb = GetComponent<Rigidbody2D>();
+    sr = GetComponent<SpriteRenderer>();
+    anim = GetComponent<Animator>();
 
-		isGrounded = Physics2D.OverlapBox(new Vector2(feet.position.x, feet.position.y), new Vector2(feetWidth, feetHeigth), 360.0f, whatIsGround); 	
-
-		
-		float horizontalInput = Input.GetAxisRaw("Horizontal"); // -1: esquerda, 1:direita
-		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
-		if (horizontalPlayerSpeed != 0){
-			MoveHorizontal(horizontalPlayerSpeed);
-		}
-
-		else {
-			StopMovingHorizontal();
-		}
-
-		if(Input.GetButtonDown("Jump")) {
-			Jump();
-		}
-
-		ShowFalling();
-
-	}
-
-	void MoveHorizontal(float speed) {
-		rb.velocity = new Vector2(speed, rb.velocity.y);
-
-		if (speed < 0f) {
-			sr.flipX = true;
-		}
-		else if (speed > 0f) {
-			sr.flipX = false;
-			}
-
-			if (!isJumping) {
-				anim.SetInteger("State", 2);
-			}
-
-anim.SetInteger("State", 2);
-
-		}
+}
 
 
-	void StopMovingHorizontal() {
-		rb.velocity = new Vector2(0f, rb.velocity.y);
-		if (!isJumping) {
-		anim.SetInteger("State", 0);
-		
-		}
-	}
+void OnDrawGizmos (){
+    Gizmos.DrawWireCube(feet.position, new Vector3(feetWidth, feetHeight, 0f));
 
-	void ShowFalling() {
-		if (rb.velocity.y < 0f) {
-			anim.SetInteger("State", 3); 
-		}
-	}
+}
+
+// Update is called once per frame
+void Update () {
+
+    isGrounded = Physics2D.OverlapBox(new Vector2(feet.position.x, feet.position.y),new Vector2(feetWidth, feetHeight), 360.0f, whatIsGround);
+
+float horizontalInput = Input.GetAxisRaw("Horizontal"); // -1: esquerda, 1: direita 
+float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
+if (horizontalPlayerSpeed != 0){
+    MoveHorizontal(horizontalPlayerSpeed);
+}
+else {
+    StopMovingHorizontal();
+    }
+
+    if (Input.GetButtonDown("Jump")){
+        Jump();
+    }
+    ShowFalling();
+}
+
+void MoveHorizontal(float speed) {
+    rb.velocity = new Vector2(speed, rb.velocity.y);
+
+    if (speed < 0f) {
+        sr.flipX = true;
+    }
+    else if (speed > 0f){
+        sr.flipX = false;
+    }
+
+    if (!isJumping){
+        anim.SetInteger("State", 2);
+    }
+}
+void StopMovingHorizontal() {
+    rb.velocity = new Vector2 (0f, rb.velocity.y);
+     if (!isJumping){
+       anim.SetInteger("State",0);
+     }
+  }
 
 
+  void ShowFalling(){
+      if (rb.velocity.y < 0f) {
+          anim.SetInteger("State", 3);
 
-	void Jump() {
-		if (isGrounded) { 	
-		isJumping = true;
-		rb.AddForce(new Vector2(0f, jumpSpeed));
-		anim.SetInteger("State", 1);
+      }
+  }
+  void Jump(){
+      if (isGrounded){
+      isJumping = true;
+      rb.AddForce(new Vector2(0f, jumpSpeed));
+      anim.SetInteger("State", 1);
 
-		}
-	}
+      Invoke("EnableDoubleJump", delayForDoubleJump);
+      }
+      if (canDoubleJump && !isGrounded) {
+         rb.velocity = Vector2.zero;
+         rb.AddForce(new Vector2(0f, jumpSpeed));
+         anim.SetInteger("State", 1);
+         canDoubleJump = false;
 
-	void OnCollisionEnter2D(Collision2D other) {
-		if (other.gameObject.layer == LayerMask.NameToLayer("Ground")) {
-			isJumping = false;
-		}
-	}
+      }
+  }
+
+  void EnableDoubleJump (){
+      canDoubleJump = true;
+  }
+
+  void OnCollisionEnter2D(Collision2D other) {
+      if (other.gameObject.layer == LayerMask.NameToLayer("Ground")){
+          isJumping = false;
+
+      }
+  }
+
 }
