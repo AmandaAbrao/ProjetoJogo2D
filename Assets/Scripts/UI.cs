a6be5438eafd5823744f154ca24f28427d7d5481
AmandaAbrao <amandaabrao55@gmail.com>
2017-11-28 11:43:46 -0200

    Level complete

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -39,9 +39,26 @@ public class UI {
 	public GameObject gameOverPanel;
 
 	}
+
+	[Serializable]
+
+	public class LevelComplete {
+
+	[Header("Text")]
+
+	public Text txtCoinCount;
+	public Text txtTimer;
+
+	[Header("Other")]
+
+	public GameObject levelCompletePanel;
+
+	}
+	
 	
 
 	public HUD hud;
 	public GameOver gameOver;
+	public LevelComplete levelComplete;
 
 }
